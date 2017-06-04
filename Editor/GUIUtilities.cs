cb8a2610cf64c6b1939a79a476743629f484cd99
Jesse Stiller <aquageneral@hotmail.com>
2017-06-04 12:57:00 +1000

    Removed worse version of auto configure control

--- Editor/GUIUtilities.cs
+++ Editor/GUIUtilities.cs
@@ -10,44 +10,6 @@ using UnityEngine;
 namespace JesseStiller.Enjoined {
     public static class GUIUtilities {
         public static void DrawConnectedAnchorProperty(SerializedProperty connectedAnchor, SerializedProperty autoConfigureConnectedAnchor) {
-            Rect controlRect = EditorGUILayout.GetControlRect();
-
-            GUIStyle labelZeroRightPadding = new GUIStyle(GUI.skin.label);
-            labelZeroRightPadding.padding.right = 0;
-
-            GUIStyle labelWithoutPadding = new GUIStyle(GUI.skin.label);
-            labelWithoutPadding.padding.left = labelWithoutPadding.padding.right = 0;
-
-            GUIStyle toggleWithoutPadding = new GUIStyle(GUI.skin.toggle);
-            toggleWithoutPadding.padding.right = 0;
-
-            float connectedAnchorLabelWidth = labelZeroRightPadding.CalcSize(new GUIContent("Connected Anchor (")).x;
-            float rightBracketWidth = labelWithoutPadding.CalcSize(new GUIContent(")")).x;
-            float autoLabelWidth = toggleWithoutPadding.CalcSize(new GUIContent("Auto")).x;
-
-            GUI.Label(controlRect, "Connected Anchor (", labelZeroRightPadding);
-            bool newAutoConfigure = GUI.Toggle(new Rect(controlRect.x + connectedAnchorLabelWidth, controlRect.y, autoLabelWidth, controlRect.height), autoConfigureConnectedAnchor.boolValue, "Auto", toggleWithoutPadding);
-            GUI.Label(new Rect(controlRect.x + connectedAnchorLabelWidth + autoLabelWidth, controlRect.y, controlRect.width, controlRect.height), ")", labelWithoutPadding);
-            
-            if(autoConfigureConnectedAnchor.boolValue != newAutoConfigure) {
-                autoConfigureConnectedAnchor.boolValue = newAutoConfigure;
-
-                // Forcibly reset the connectedAnchor point if it's set to auto configure.
-                if(newAutoConfigure == true) {
-                    Joint joint = (Joint)connectedAnchor.serializedObject.targetObject;
-                    joint.autoConfigureConnectedAnchor = true; // The property must be set to true right now for connectedAnchor to be updated.
-                    connectedAnchor.vector3Value = joint.connectedAnchor;
-                }
-            }
-
-            Rect fillRect = new Rect(controlRect);
-            fillRect.xMin = EditorGUIUtility.labelWidth + 13f;
-            using(new EditorGUI.DisabledGroupScope(autoConfigureConnectedAnchor.boolValue)) {
-                EditorGUI.PropertyField(fillRect, connectedAnchor, GUIContent.none);
-            }
-
-            EditorGUILayout.Space();
-
             Rect controlRect2 = EditorGUILayout.GetControlRect();
             Rect fillRect2 = EditorGUI.PrefixLabel(controlRect2, new GUIContent("Connected Anchor Mode"));
             autoConfigureConnectedAnchor.boolValue = GUI.Toolbar(fillRect2, autoConfigureConnectedAnchor.boolValue ? 1 : 0, new GUIContent[] { new GUIContent("User"), new GUIContent("Automatic") }, EditorStyles.radioButton) == 1;
